14320a6e22e59297d8a4dc0ba69d3f6ffcae9ff6
wesleson22 <wesllesonsowza@gmail.com>
2021-08-29 19:12:45 -0300

    Tela 2 sem Icon

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,19 +1,32 @@
 import React from 'react';
-import {SafeAreaView, Text, View, StyleSheet, Image, TouchableOpacity, Touchable} from 'react-native';
+import {SafeAreaView,
+        Text,
+        View, 
+        StyleSheet, 
+        Image, 
+        TouchableOpacity, 
+        Touchable, Dimensions} 
+        from 'react-native';
 import colors from '../../styles/colors';
 import wateringImg from '../assets/watering.png';
-import {Button} from '../components/Button';
+
 export function Welcome(){
     return(
         <SafeAreaView style={styles.container}>
             <Text style={styles.title}>Gerencie{'\n'}suas plantas{'\n'}de forma fácil</Text>
-            <Image source={wateringImg} style={styles.image}/>
+
+            <Image source={wateringImg} style={styles.image} resizeMode="contain"/>
+
             <Text style={styles.subtitle}>
                 Não esqueça mais de regar suas plantas.
                 Nós cuidamos de lembrar você sempre que precisar!. 
             </Text>
-           <Button/>
-           
+            <TouchableOpacity style={styles.button} activeOpacity={0.7}>
+        <Text style={styles.buttontex} >
+            >
+        </Text>
+         </TouchableOpacity>
+        
         </SafeAreaView>
     )
 }
@@ -22,7 +35,7 @@ const styles =StyleSheet.create({
     container:{
         flex:1,
         alignItems: 'center',
-        justifyContent: 'space-between'
+        justifyContent: 'space-around'
     },
     title:{
         fontSize:32,
@@ -37,6 +50,11 @@ const styles =StyleSheet.create({
         paddingHorizontal: 20,
         color: colors.heading
     },
+      
+    image:{
+        
+        height: Dimensions.get('window').width*0.7,
+    },
     button:{
         backgroundColor:colors.green,
         justifyContent: 'center',
@@ -44,13 +62,8 @@ const styles =StyleSheet.create({
         borderRadius: 16,
         marginBottom: 10,
         height: 56,
+       width: 56,
        
-        paddingHorizontal:10 
-
-    },
-    image:{
-        width: 292,
-        height: 292,
     },
     buttontex:{
         color: colors.white,
