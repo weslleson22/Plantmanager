5344ac6f7b2e96960d09b0d78f14c84a67c0d5d2
wesleson22 <wesllesonsowza@gmail.com>
2021-09-12 23:29:18 -0300

    Trabalhando com a imagem de perfil 17m52s

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,10 +2,15 @@ import React from "react";
 import {Text, View, StyleSheet, Image} from "react-native";
 import colors from "../styles/colors";
 import {getStatusBarHeight} from 'react-native-iphone-x-helper';
+import userImg from '../assets/perfil.png';
 export function Header(){
     return(
         <View style={styles.container}>
-
+            <View>
+            <Text>Olá,</Text>
+            <Text>Wesleson</Text>
+            </View>
+            <Image source={userImg}/>
         </View>
     )
 }
@@ -18,6 +23,6 @@ const styles = StyleSheet.create({
         alignItems: 'center', 
         paddingVertical: 20,
         marginTop: getStatusBarHeight(),
-        backgroundColor: colors.red
+        
     }
 })
