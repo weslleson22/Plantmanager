a791035c41ae8fa306023b9171790b51088fe65c
wesleson22 <wesllesonsowza@gmail.com>
2021-09-24 23:49:27 -0300

    as

---
+++ src/components/EnviromentButton.tsx
@@ -0,0 +1,63 @@
+import { Jost_400Regular } from "@expo-google-fonts/jost";
+import React from "react";
+import {StyleSheet, Text} from "react-native";
+
+import {RectButton, RectButtonProps} from "react-native-gesture-handler";
+
+import colors from "../styles/colors";
+import fonts from "../styles/fonts";
+
+interface EnviromentButtonProps extends RectButtonProps{
+    title: string;
+    active?: boolean;
+
+}
+export function EnviromentButton({
+    title,
+    active=false,
+    ...rest
+}: EnviromentButtonProps){
+    return(
+        <RectButton
+        style={
+            [styles.container,
+            active && styles.containerActive
+            
+            ]}
+        {... rest}>
+    <Text style={
+        [styles.text,
+            active && styles.textActive
+        
+        
+        ]}>
+        {title}
+    </Text>
+
+        </RectButton>
+    );
+}
+const styles = StyleSheet.create({
+    container:{
+        backgroundColor: colors.shape,
+        width: 76,
+        height: 40,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 12
+ 
+    }, 
+    containerActive:{
+        backgroundColor: colors.heading,
+
+    },
+    text:{
+        color: colors.heading,
+        fontFamily: fonts.text
+
+    },
+    textActive:{
+        fontFamily: fonts.heading,
+        color: colors.green,
+    }
+})
--- src/pages/PlantSelect.tsx
+++ src/pages/PlantSelect.tsx
@@ -5,13 +5,26 @@ import {
     StyleSheet,
     Text
 } from "react-native";
+import { EnviromentButton } from "../components/EnviromentButton";
 import { Header } from "../components/Header";
 import colors from "../styles/colors";
+import fonts from "../styles/fonts";
 
 export function PlantSelect(){
     return(
         <View style={styles.container}>
-            <Header/>
+            <View style={styles.header}>
+                <Header/>
+                <Text style={styles.title}>
+                    Em qual ambiente
+                </Text>
+                <Text style={styles.subtitle}>
+                    Você quer colocar sua planta?
+                </Text>
+               
+            </View>
+            <EnviromentButton title="Cozinha"/>
+            
         </View>
     )
 } 
@@ -19,5 +32,22 @@ const styles = StyleSheet.create({
     container:{
         flex: 1,
         backgroundColor: colors.background
-    }
+    },
+    header:{
+        paddingHorizontal:30
+    },
+    title:{
+        fontSize: 17,
+        color: colors.heading,
+        fontFamily: fonts.heading,
+        lineHeight:20,
+        marginTop:15
+
+    },
+    subtitle:{
+        fontFamily: fonts.text,
+        fontSize: 17,
+        lineHeight: 20,
+        color: colors.heading
+    },
 })
